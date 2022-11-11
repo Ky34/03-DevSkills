b3def6f6051b9914afc96ec685d5c9919483353d
Aleksei Kozlov <Ky34.aleksei.com>
2022-11-11 17:27:57 +0300

    init

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// для настройки создаем файл с название .prettierrc
+// {
+//    "singleQuote": true    настройка кавычек
+//    "arrowParens": "avoid"   настройка скобок стрелочной функции(убирает скобки если входящий парметр 1)
+// }
+
+const myVar = 'asdq';
+while (false) {}
+
+const calcAge = birthYear => 2021 - birthYear;
+
+console.log();
